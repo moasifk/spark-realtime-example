3be4e3335ce26b9a5918ca6f524593a7443aa4d3
Mohammed Asif <asif.the.winner@gmail.com>
2018-01-03 21:00:23 +0530

    Updated comments: added spark-submit command

--- spark-realtime-parent/spark-realtime-core/src/main/java/com/asif/spark/realtime/SparkRealtimeExample.java
+++ spark-realtime-parent/spark-realtime-core/src/main/java/com/asif/spark/realtime/SparkRealtimeExample.java
@@ -1,9 +1,17 @@
 package com.asif.spark.realtime;
 
+import java.io.Serializable;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Properties;
 
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.function.Function;
 import org.apache.spark.streaming.Durations;
@@ -15,8 +23,31 @@ import org.apache.spark.streaming.kafka.KafkaUtils;
 import kafka.serializer.StringDecoder;
 import scala.Tuple2;
 
-public class SparkRealtimeExample {
+/**
+ ** 
+ *This program can be executed by following command from your local build jar location
+ * spark-submit --master yarn --deploy-mode cluster --class com.asif.spark.realtime.SparkRealtimeExample 
+ * spark-realtime-core-jar-with-dependencies.jar localhost:9092 localhost:2181 SparkTest
+ * 
+ */
+public class SparkRealtimeExample implements Serializable {
 
+	private final static String BOOTSTRAP_SERVERS =
+            "localhost:9092";
+	private final static String TOPIC = "kafka-streaming-output";
+	
+	private static Producer<String, String> createProducer() {
+        Properties props = new Properties();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,
+                                            BOOTSTRAP_SERVERS);
+        props.put(ProducerConfig.CLIENT_ID_CONFIG, "KafkaSparkExampleProducer");
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
+        		StringSerializer.class.getName());
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
+                                    StringSerializer.class.getName());
+        return new KafkaProducer<String, String>(props);
+    }
+	
 	public static void main(String args[]) {
 		 SparkConf conf = new SparkConf()
 	                .setAppName("SparkStreamingKafkaExample");
@@ -42,10 +73,18 @@ public class SparkRealtimeExample {
 	            topicsSet
 	        );
 	        
+	        final Producer<String, String> producer = createProducer();
+	        
 	        // Get the lines, split them into words, count the words and print
 	        JavaDStream<String> lines = messages.map(new Function<Tuple2<String, String>, String>(){
 				public String call(Tuple2<String, String> line) throws Exception {
 					System.out.println(line._2());
+					
+					final ProducerRecord<String, String> record =
+		                      new ProducerRecord<String, String>(TOPIC, "key",
+		                    		  line._2());
+		              RecordMetadata metadata = producer.send(record).get();
+
 					return line._2();
 				}
 	        	
